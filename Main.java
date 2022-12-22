c120791d775a10467760b1563dcd4c261f5395c0
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-22 12:45:26 +0300

    add methods print and read

--- Main.java
+++ Main.java
@@ -1,26 +1,26 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Person irina = new Person("Ирина");
+        // Person irina = new Person("Ирина");
         Person vasya = new Person("Вася");
         Person masha = new Person("Маша");
         Person liza = new Person("Елизавета");
         Person egor = new Person("Егор");
-        // Person jane = new Person("Женя");
-        // Person ivan = new Person("Ваня");
+
         GeoTree gt = new GeoTree();
-        gt.append(irina, vasya);
-        gt.append(irina, masha);
-        gt.append(irina, liza);
-        gt.append(irina, egor);
-        // gt.append(vasya, jane);
-        // gt.append(vasya, ivan);
-        gt.appendSibling(vasya, masha);
-        gt.appendSibling(vasya, liza);
-        gt.appendSibling(vasya, egor);
-        gt.appendSibling(masha, liza);
-        gt.appendSibling(masha, egor);
-        gt.appendSibling(liza, egor);
+        // родитель - ребенок
+        // gt.append(irina, vasya, Relationship.parent, Relationship.children);
+        // gt.append(irina, masha, Relationship.parent, Relationship.children);
+        // gt.append(irina, liza, Relationship.parent, Relationship.children);
+        // gt.append(irina, egor, Relationship.parent, Relationship.children);
+        // брат(сестра) - брат(сестра)
+        gt.append(vasya, masha, Relationship.sibling, Relationship.sibling);
+        gt.append(vasya, liza, Relationship.sibling, Relationship.sibling);
+        gt.append(vasya, egor, Relationship.sibling, Relationship.sibling);
+        gt.append(masha, liza, Relationship.sibling, Relationship.sibling);
+        gt.append(masha, egor, Relationship.sibling, Relationship.sibling);
+        gt.append(liza, egor, Relationship.sibling, Relationship.sibling);
 
         // Вывести на экран всех детей Ирины
         // System.out.println(new Reserch(gt).spend(irina,
@@ -28,11 +28,15 @@ public class Main {
 
         // Вывести на экран всех братьев и сестер Маши
         System.out.println("Вывести на экран всех братьев и сестер Маши:");
-        var a = new Reserch(gt).spend(masha, Relationship.sibling);
-        System.out.println(a);
-        System.out.println("");
+        ArrayList<String> siblingMasha = new Reserch(gt).spend(masha, Relationship.sibling);
+        System.out.println(siblingMasha);
+        System.out.println();
+        // Вывести на экран всех братьев и сестер Маши, чьи имена начинаются на Е
         System.out.println("Вывести на экран всех братьев и сестер Маши, чьи имена начинаются на Е: \n"
-                + new Reserch(gt).getNameWhoseFirstLetterIsE(a));
+                + new Reserch(gt).getNameWhoseFirstLetterIsE(siblingMasha));
+        System.out.println();
+        gt.print("fileGeoTree.txt");
+        gt.read("fileGeoTree.txt");
 
     }
 
