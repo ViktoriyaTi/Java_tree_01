dae7ecccf4f17f15a4731c3814a43b8ffa48c9d3
ViktoriyaTim <ViktoriyaTimon@gmail.com>
2022-12-26 12:41:02 +0300

    add person height, implements comparable

--- Person.java
+++ Person.java
@@ -1,13 +1,27 @@
-
-public class Person {
+public class Person implements Comparable<Person> {
     public String fullName;
+    private int height;
 
-    public Person(String fullName) {
+    public Person(String fullName, int height) {
         this.fullName = fullName;
+        this.height = height;
     }
 
     public String getFullName() {
         return fullName;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int setHeight(int height) {
+        return this.height = height;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.getHeight() - o.getHeight();
     }
 
 }
